05aba7d1a3cc5f1e9423b6e80df5dd1145cedb21
LGoldberg2424 <73358918+LGoldberg2424@users.noreply.github.com>
2020-10-26 13:01:41 -0700

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,9 @@
 import random
 def main():
   roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+  if roll == 1:
+  print(f'You rolled a {roll}! Critical Fail')
+  else print(f'You rolled a {roll}')
 
 if __name__== "__main__":
   main()
